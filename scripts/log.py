3adb0321dcd2a5e54313240c9af71f951d792116
Austin Tirrell <101142641+austintirrell@users.noreply.github.com>
2024-02-08 16:37:28 -0600

    Build logging script with error collecting functionality

---
+++ scripts/log.py
@@ -0,0 +1,33 @@
+import os
+import logging
+
+
+# Collects all `logger.error()` calls
+ERROR_COLLECTOR = []
+
+
+class CustomLogger(logging.Logger):
+    def error(self, msg, *args, **kwargs):
+        ERROR_COLLECTOR.append(msg)
+        super().error(msg, *args, **kwargs)
+
+
+logging.setLoggerClass(CustomLogger)
+logging.basicConfig(level=logging.INFO, format='%(asctime)s [%(levelname)s] %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+
+
+def setup_logger(name, log_file, level=logging.INFO):
+    FORMATTER = logging.Formatter('%(asctime)s [%(levelname)s] %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+    HANDLER = logging.FileHandler(log_file)        
+    HANDLER.setFormatter(FORMATTER)
+    LOGGER = logging.getLogger(name)
+    LOGGER.setLevel(level)
+    LOGGER.addHandler(HANDLER)
+    return LOGGER
+
+
+# Set up loggers here...
+MAIN_LOG = setup_logger('main_logger', os.path.join('logs', 'main.log'))
+TEST_LOG = setup_logger('test_logger', os.path.join('logs', 'test.log'))
+
+HELPER_LOG = setup_logger('helper_logger', os.path.join('logs', 'helper.log'))
